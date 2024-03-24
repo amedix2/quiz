c88c2332d5144c284635c7b28b12b9f77e0fcf97
amedix <amedix2@gmail.com>
2024-03-24 23:16:31 +0400

    added basic flask routes

--- app/flask_server.py
+++ app/flask_server.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=['GET'])
+def main():
+    return render_template('main.html')
+
+
+@app.route('/question/<int:num>')
+def questions(num):
+    return render_template('question.html', num=num)
+
+
+@app.route("/results")
+def results():
+    return render_template('results.html')
 
 
 if __name__ == "__main__":
